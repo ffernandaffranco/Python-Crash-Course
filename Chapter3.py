74d2f443616df30bbf74fdd8405bc5d5e9285d3f
ffernandaffranco <130008309+ffernandaffranco@users.noreply.github.com>
2023-08-18 23:49:17 -0300

    Update Chapter3.py

--- Chapter3.py
+++ Chapter3.py
@@ -1,1 +1,6 @@
-print("Hello world!")
+# bicicletas[-1] accede a la última bicicleta de la lista
+# append() agrega un elemento a lo último de la lista
+# insert() inserta un elemento en la posición deseada en la lista
+# del elimina un elemento de la lista. Ej: del motorcycles[0]
+# pop() elimina el último elemento de la lista pero te sigue dejando trabajar con él
+# remove() elimina un elemento de la lista por su valor
