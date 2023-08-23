46542d879877f1eac933a063bbcceb44c9d9fad6
ffernandaffranco <fernandafrancol@protonmail.com>
2023-08-23 20:11:02 -0300

    Update Chapter 6

---
+++ Chapter 6 - Dictionaries/glossary2.py
@@ -0,0 +1,10 @@
+glossary = {
+    'variable': 'variables are used to store values',
+    'string': 'series of characters, surrounded by single or double quotes',
+    'list': 'stores a series of items in particular order',
+    'if': 'used to test for particular conditions and respond appropiately',
+    'while': 'repeats a block of code as long as certain condition is true'
+}
+
+for key, value in glossary.items():
+    print(f"{key.title()}: {value.capitalize()}.")
---
+++ Chapter 6 - Dictionaries/rivers.py
@@ -0,0 +1,16 @@
+rivers = {
+    'nile': 'egypt',
+    'amazon': 'peru',
+    'mississippi': 'united states'
+}
+
+for river, country in rivers.items():
+    print(f"The {river.title()} runs through {country.title()}.")
+
+print("\nThe following rivers are included in this data set:")
+for river in rivers.keys():
+    print(f"{river.title()}.")
+
+print("\nThe following countries are included in this data set:")
+for country in rivers.values():
+    print(f"{country.title()}")
